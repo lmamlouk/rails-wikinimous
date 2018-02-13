548269f0ee96cdfe9c94964532654f2ada3811e8
Lamia Mamlouk <lamia.mamlouk@gmail.com>
2018-02-13 17:11:05 +0100

    exericice 2

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+
+  end
+
+
+  def show
+       # GET /restaurants/:id
+    @article = Article.find(params[:id])
+
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:article).permit(:title, :content)
+  end
+
+end
